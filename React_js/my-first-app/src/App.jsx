738f6ce30ea0aaba4c8e824d8b2889263b9e91a7
subeerkumar007 <subeerk491@gmail.com>
2025-11-04 10:27:43 +0530

    latest

---
+++ React_js/my-first-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Message from './message.jsx'
+
+function App() {
+ 
+
+  return (
+   <div className="container">
+    
+    <h1>Hello World</h1>
+    <Message />
+   </div>
+  )
+}
+export default App
---
+++ React_js/my-first-app/src/message.jsx
@@ -0,0 +1,5 @@
+function Message() {
+  return <h1>This is message section</h1>
+}
+
+export default Message
